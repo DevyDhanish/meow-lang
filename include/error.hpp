14a205b30e0affae915d9f5d77590f4a40991da6
DevyDhanish <dhanishkariyatmain@gmail.com>
2023-10-24 12:58:47 +0530

    fix

--- include/error.hpp
+++ include/error.hpp
@@ -1,21 +1,21 @@
-// #pragma once
+#pragma once
 
-// #include "tree.hpp"
-// #include <unordered_map>
-// #include <vector>
+#include "tree.hpp"
+#include <unordered_map>
+#include <vector>
 
-// typedef enum ERROR_TYPE 
-// {
-//     _E_SYNTAX_ERROR,            // 0
-//     _E_TYPE_ERROR,              // 1
-//     _E_CMP_ERROR,               // 2
-//     _E_UNKNOW_TOKEN_ERROR,      // 3
-//     _E_INDENTATION_ERROR,       // 4
-// } Error_T;
+typedef enum ERROR_TYPE 
+{
+    _E_SYNTAX_ERROR,            // 0
+    _E_TYPE_ERROR,              // 1
+    _E_CMP_ERROR,               // 2
+    _E_UNKNOW_TOKEN_ERROR,      // 3
+    _E_INDENTATION_ERROR,       // 4
+} Error_T;
 
-// extern std::unordered_map<Error_T, std::string> error_table;
+extern std::unordered_map<Error_T, std::string> error_table;
 
-// void displayError(Error_T error_type, std::string _line, size_t line_number);
+void displayError(Error_T error_type, std::string _line, size_t line_number);
 // void checkSyntax(std::vector<Token> tokens);
 // void checkInt(Token);
 // void checkEqu(Token);
--- src/error.cpp
+++ src/error.cpp
@@ -1,27 +1,27 @@
-// #include "../include/error.hpp"
-// #include <unordered_map>
-// #include "../include/token.hpp"
-// #include "../include/vm.hpp"
-// #include <iostream>
-// #include <vector>
+#include "../include/error.hpp"
+#include <unordered_map>
+#include "../include/token.hpp"
+#include "../include/vm.hpp"
+#include <iostream>
+#include <vector>
 
-// std::unordered_map<Error_T, std::string> error_table = 
-// {
-//     {_E_SYNTAX_ERROR, "Syntax Error at %lu : %s"},
-//     {_E_CMP_ERROR, "Comparison Error at %lu : %s"},
-//     {_E_TYPE_ERROR, "Type Error at %lu : %s"},
-//     {_E_INDENTATION_ERROR, "Indentation Error at %lu : %s"},
-//     {_E_UNKNOW_TOKEN_ERROR, "Unknow Token at %lu : %s"},
-// };
+std::unordered_map<Error_T, std::string> error_table = 
+{
+    {_E_SYNTAX_ERROR, "Syntax Error at %lu : %s"},
+    {_E_CMP_ERROR, "Comparison Error at %lu : %s"},
+    {_E_TYPE_ERROR, "Type Error at %lu : %s"},
+    {_E_INDENTATION_ERROR, "Indentation Error at %lu : %s"},
+    {_E_UNKNOW_TOKEN_ERROR, "Unknow Token at %lu : %s"},
+};
 
-// int counter = 0;
-// std::vector<Token> TOKENS;
+int counter = 0;
+std::vector<Token> TOKENS;
 
-// void displayError(Error_T error_type, std::string _line, size_t line_number) {
-//     printf(error_table[error_type].c_str(), line_number, _line.c_str());
-//     printf("\n");
-//     exit(0);
-// }
+void displayError(Error_T error_type, std::string _line, size_t line_number) {
+    printf(error_table[error_type].c_str(), line_number, _line.c_str());
+    printf("\n");
+    exit(0);
+}
 
 // Token advance()
 // {
